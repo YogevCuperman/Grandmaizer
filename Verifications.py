a048cdc3374ebb880a6e97cf87eb3d5917e8c2dc
Yogev Cuperman <107213417+YogevCuperman@users.noreply.github.com>
2023-05-11 19:06:37 +0300

    more adjustments

--- Verifications.py
+++ Verifications.py
@@ -1,3 +1,29 @@
 def id_check(prompt: str) -> bool:
     prompt = ''.join(prompt.split())
-    return len(prompt) == 9 and prompt.isnumeric()
+    return len(prompt) == 9 and prompt.isnumeric() and validate_israeli_id(prompt)
+
+
+def validate_israeli_id(id_number):
+    # mult every other num and put in arr
+    arr = []
+    for i in range(len(id_number)):
+        n = int(id_number[i])
+        if i % 2 == 0:
+            arr.append(n)
+        else:
+            n2 = n * 2
+            if n2 > 9:
+                n1 = 1 + (n2 % 10)
+                arr.append(n1)
+            else:
+                arr.append(n2)
+
+            # Sum up all the numbers from the previous step
+    total = 0
+    for k in arr:
+        total += k
+
+    if total % 10 == 0:
+        return True
+    else:
+        return False
--- VoiceRecognition.py
+++ VoiceRecognition.py
@@ -7,14 +7,15 @@ recognizer = speech_recognition.Recognizer()
 tts = pyttsx3.init()
 voices = tts.getProperty('voices')
 rate = tts.getProperty('rate')
-tts.setProperty('voice', voices[1].id)
-tts.setProperty('rate', rate - 75)
+tts.setProperty('voice', voices[1].id)  # female voice
+tts.setProperty('rate', rate - 75)      # slowing it down
+
 
 def listen() -> str:
     global recognizer
     try:
         with speech_recognition.Microphone() as mic:
-            recognizer.adjust_for_ambient_noise(mic, duration=0.1)
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
             audio = recognizer.listen(mic)
             text = recognizer.recognize_google(audio).lower()
             return text
@@ -27,5 +28,6 @@ def say(response: str):
     tts.say(response)
     tts.runAndWait()
 
+
 def say_again():
-    say("i didn't catch that. can you say that again please?")
+    say("I didn't catch that. Can you say it again please?")
